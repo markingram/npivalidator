a30f15168c3227db6c72f6eb52b8532708a797d3
Mark Ingram <mark.ingram@nanthealth.com>
2018-07-05 10:39:24 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace npivalidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>> Main");
+            string fileContent = readFirstLineOfFile("npis.csv");
+            Console.WriteLine("<<< Main");
+        }
+
+        private static string readFirstLineOfFile(string filename)
+        {
+            string lineOne;
+            FileStream fileStream = new FileStream(filename, FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                lineOne = reader.ReadLine();
+                Console.WriteLine(lineOne);
+            }
+            fileStream.Close();
+            return lineOne;
+        }
+    }
+}
